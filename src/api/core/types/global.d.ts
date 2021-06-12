bdb5e954b10a40dbf23c324cdbea4ff972e39ef8
Cataldo Cianciaruso <cianciaruso.cataldo@gmail.com>
2021-06-12 23:04:33 +0200

    types cleanup

--- src/api/core/types/global.d.ts
+++ src/api/core/types/global.d.ts
@@ -1,15 +1,6 @@
-declare type HeaderType = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
-declare type Status = "EMPTY" | "BUSY" | "READY" | "ERROR";
-declare type ModalType = "EMPTY" | "CONFIRM" | "INFO";
 declare type RouteKey = "HOME" | "INFO" | "PROJECTS";
-declare type AnimationKey = "NONE" | "FADE" | "SLIDE";
-declare type UnionOf<T> = T[keyof T];
-declare type DynamicImportType = () => Promise<{
-  default: React.ComponentType<any>;
-}>;
-declare type LazyComponentType = React.LazyExoticComponent<
-  React.ComponentType<any>
->;
+declare type SupportedLanguage = "en" | "es" | "de" | "fr" | "it";
+
 declare interface Skill {
   name: string;
   expertise: number;
@@ -22,7 +13,7 @@ declare interface Work {
   start: string;
   end?: string;
 }
-declare type SupportedLanguage = "en" | "es" | "de" | "fr" | "it";
+
 declare interface Widget {
   compact?: boolean;
 }
