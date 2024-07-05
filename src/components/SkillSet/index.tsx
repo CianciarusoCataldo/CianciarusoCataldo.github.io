c10a7f3631a62a19b2cca8e5d875886ab954b969
CianciarusoCataldo <cianciaruso.cataldo@gmail.com>
2024-07-05 10:03:50 +0200

    updates

--- src/components/SkillSet/index.tsx
+++ src/components/SkillSet/index.tsx
@@ -4,10 +4,11 @@ import { SkillSetProps } from "./types";
 const SkillSet = ({ skills }: SkillSetProps) => (
   <div className="flex flex-col px-3">
     {skills.map((skill) => (
-      <div className="flex flex-col py-1 mb-7">
+      <div className="flex flex-col items-center py-1 mb-7">
         <Label className="m-auto text-2xl text-center px-3">{skill.name}</Label>
         <Slider
           style={{
+            width: "100%",
             height: "fit-content",
           }}
           className="mt-3"
